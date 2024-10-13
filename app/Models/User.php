cc67f93d3076455041632b580363e23b24df5b69
Samuel <s.mendezgarcia@student.helmo.be>
2024-10-13 16:39:26 +0200

    back login + token api

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
 
 /**
  * Class User
@@ -39,6 +41,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
  */
 class User extends Authenticatable
 {
+    use HasApiTokens, HasFactory;
+
 	protected $table = 'user';
 	protected $primaryKey = 'userId';
 	public $timestamps = false;
