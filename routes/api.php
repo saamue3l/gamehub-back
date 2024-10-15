c280ac1cc18e7f2581f36185a6d0c63679149833
Raphael Montesanti <r.montesanti@student.helmo.be>
2024-10-15 16:22:55 +0200

    Creating main files for matchming API & Tests

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MatchController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('logout', [AuthController::class, 'logout']);
+Route::post('matchmaking', [MatchController::class, 'matchmaking']);
