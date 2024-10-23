e0f641c057d1656fcb73e1b00e5fee93d43b7ed6
Samuel <s.mendezgarcia@student.helmo.be>
2024-10-23 17:32:22 +0200

    Adjusted AuthController.php and cors rules

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,29 +9,19 @@ class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        //décommenter une fois le front mis en place
-        //$credentials = $request->only('email', 'password');
-
-        //supprimer une fois le front mis en place
-        $credentials = [
-            'email' => 'lemuel77@example.net',
-            'password' => 'password123',
-        ];
+        $credentials = $request->only('email', 'password');
 
         if (Auth::attempt($credentials)) {
-
-            $user = Auth::user(); //user identifié
+            $user = Auth::user();
 
             $token = $user->createToken('token-name')->plainTextToken;
 
-            // Retourner le token au front-end pour les prochaines requêtes
             return response()->json([
                 'message' => 'Connexion réussie',
                 'token' => $token,
                 'user' => $user,
             ]);
         } else {
-            // Si les infos d'auth sont incorrectes
             return response()->json([
                 'message' => 'Échec de la connexion, informations incorrectes.',
             ], 401);
